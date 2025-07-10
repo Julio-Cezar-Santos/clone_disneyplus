533494635b24e2caec41efcd045f37ddb1807d1a
Julio Cezar Trajano dos Santos <jcezar0123@hotmail.com>
2025-07-09 22:27:13 -0300

    faq

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-const buttons = document.querySelectorAll('[data-tab-button]');
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
 
-    for (let i=0 ; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function(botao) {
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function (botao) {
             const abaAlvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`);
             escondeTodasAbas();
@@ -12,19 +13,31 @@ const buttons = document.querySelectorAll('[data-tab-button]');
             botao.target.classList.add("shows__tabs__button--is-active");
         });
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+
 });
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoActive() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    for (let i=0; i < buttons.length; i++) {
+    for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove("shows__tabs__button--is-active");
     }
 }
 
-function escondeTodasAbas(){
+function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
-    for (let i=0; i < tabsContainer.length; i++) {
+    for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove("shows__list--is-active");
     }
 }
